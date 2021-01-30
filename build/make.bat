e635fcac63b29366356312ac04bf05708e74dd74
Matthew McCord <mattm.eternal@gmail.com>
2021-01-29 21:33:15 -0800

    move run.bat files to build folder

--- build/make.bat
+++ build/make.bat
@@ -3,12 +3,12 @@ cls
 del main.gb
 
 @REM mod2gbt [mod file name] [-c to create .c file] [memory bank number]
-mod2gbt matts_demo.mod song -c 2
+music\mod2gbt music\matts_demo.mod song -c 2
 
 @REM Compile music: output.c is created by mod2gbt
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\output.o output.c
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\gbt_player.o gbt_player.s
-c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\gbt_player_bank1.o gbt_player_bank1.s
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\output.o music\output.c
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\gbt_player.o music\gbt_player.s
+c:\gbdk\bin\lcc -Wa-l -Wl-m -Wf--debug -Wl-y -Wl-w  -c -o build\gbt_player_bank1.o music\gbt_player_bank1.s
 
 
 @REM you should pass --debug to frontend (Wf--debug)
